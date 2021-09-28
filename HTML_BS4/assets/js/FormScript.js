f162138eafb46cbb7daecda46d048488f7f576ff
SamaelTessier <samael.tessier@gmail.com>
2021-09-28 13:42:45 -0400

    fees and final price added,all 3 working

--- HTML_BS4/assets/js/FormScript.js
+++ HTML_BS4/assets/js/FormScript.js
@@ -6,31 +6,24 @@ $(document).ready(function(){
         $(".corporate").hide();
         $(".hybrid").hide();
         $(".residential").show();
+    })
 
-    });
-});
-
-$(document).ready(function(){
     $("#commercial").click(function(){
         $("#input-fields").show();
         $(".residential").hide();
         $(".corporate").hide();
         $(".hybrid").hide();
         $(".commercial").show();
-    });
-});
+    })
 
-$(document).ready(function(){
     $("#corporate").click(function(){
         $("#input-fields").show();
         $(".residential").hide();
         $(".commercial").hide();
         $(".hybrid").hide();
         $(".corporate").show();
-    });
-});
+    })
 
-$(document).ready(function(){
     $("#hybrid").click(function(){
         $("#input-fields").show();
         $(".residential").hide();
@@ -38,11 +31,9 @@ $(document).ready(function(){
         $(".corporate").hide();
         $(".hybrid").show();
     });
-});
 
 // Quote  Calculator Section
 
-$(document).ready(function(){
     var totalelevators = 0;
     let standard = 7565;
     let premium = 12345;
@@ -52,18 +43,47 @@ $(document).ready(function(){
 
     $("#quote").on("change",function(){
         if($("#residential").is(":checked")) {
-            totalelevators =($("#num-appt").val()/$("#num-floors").val()/6)}
+            var f = parseFloat($("#num-appt").val())
+            var g = parseFloat($("#num-floors").val())
+            var h = (f/g)/6;
+            var numelevators = Math.ceil(h);
+            console.log(numelevators)}   
 
-        else if($("#commercial").is(":checked")) {
-            totalelevators =($("#num-cages").val())}
+        else if($("#commercial").is(":checked")){
+            var numelevators = parseFloat($("#num-cages").val())
+            console.log(numelevators)}
 
-    //    else($("corporate").is(":checked")){
-    //        totalelevators =($("#num-occupants").val()/($("#num-floors").val()+$("#num-bsmt").val())
-    //        )}
-        
+        else if($("#corporate").is(":checked")){
+            var a = Math.ceil($("#num-floors").val())
+            var b = Math.ceil($("#num-bsmt").val())
+            var totalfloors = a + b;
+            var c = Math.ceil($("#num-occupants").val())
+            var d = 0;
+            var totaloccupants= totalfloors*c;
+            var numelevators = Math.ceil(totaloccupants/1000)
+            console.log(numelevators)}
 
+            if($("#standard-btn").is(":checked")){
+                totalelevators = numelevators*standard;
+                var fees = 0
+                var final = 0
+                fees = (totalelevators/100)*10
+                final = totalelevators+fees}
+            
+            else if($("#premium-btn").is(":checked")){
+                totalelevators = numelevators*premium;
+                var fees = 0
+                var final = 0
+                fees = (totalelevators/100)*10
+                final = totalelevators+fees}
 
+            else if($("#excelium-btn").is(":checked")){
+                totalelevators = numelevators*excelium;
+                var fees = 0
+                var final = 0
+                fees = (totalelevators/100)*10
+                final = totalelevators+fees}         
 
-        $("#ele-total").text(Math.ceil(totalelevators))
+        return $("#ele-num").text(Math.ceil(numelevators)),$("#ele-total").text(Math.ceil(totalelevators)),$("#total-fees").text(Math.ceil(fees)),$("#final-price").text(Math.ceil(final))})
+    
     })
-})
