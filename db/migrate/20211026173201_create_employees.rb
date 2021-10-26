9303f8bcdaa5644eea5c7133d24bf29913db1313
Anthonycyr <titocyr@gmail.com>
2021-10-26 13:34:35 -0400

    employee first commmit

---
+++ db/migrate/20211026173201_create_employees.rb
@@ -0,0 +1,12 @@
+class CreateEmployees < ActiveRecord::Migration[5.2]
+  def change
+    create_table :employees do |t|
+      t.integer :id
+      t.string :first_name
+      t.string :last_name
+      t.string :title
+
+      t.timestamps
+    end
+  end
+end
