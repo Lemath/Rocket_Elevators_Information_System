ca486efec5854285645c261f3d568478bd201d02
SamaelTessier <samael.tessier@gmail.com>
2021-10-28 10:33:44 -0400

    small fix for user sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'quote/index', to: 'quote#index'
-
+  devise_for :users
+  root to: 'home#index'
   #get 'back_office/index', to: 'back_office#index'
   get 'home',  to: 'application#home'
 end
