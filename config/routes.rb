e4de02cc48f0b0d8085880673daccd100a47386c
Anthonycyr <titocyr@gmail.com>
2021-10-28 15:23:25 -0400

    avant pull mathieu

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   resources :employees
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'application#index'
@@ -7,16 +6,10 @@ Rails.application.routes.draw do
   get 'residential', to: 'application#residential'
   get 'commercial', to: 'application#commercial'
 
-=======
+
   get 'quote/index', to: 'quote#index'
-<<<<<<< HEAD
   devise_for :users #for login pages
-  root to: 'home#home' #for login pages to get back home after actions
-=======
-  devise_for :users
-  root to: 'home#index'
->>>>>>> 8be5e90a1d7e4625c0ebb9b7a0008cfa81017f29
->>>>>>> 86bc82c27b7d41988113ea6d8a56f9dd764609a3
+  root to: 'home#index' #for login pages to get back home after actions
   #get 'back_office/index', to: 'back_office#index'
   get 'index',  to: 'application#index'
 end
