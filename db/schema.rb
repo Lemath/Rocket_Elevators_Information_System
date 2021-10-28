9bf20f3121dfec8f5f62e8ba6fd8179e7ab562be
Anthonycyr <titocyr@gmail.com>
2021-10-28 15:24:01 -0400

    employee change

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_26_180914) do
+ActiveRecord::Schema.define(version: 2021_10_26_201311) do
 
   create_table "employees", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.integer "Userid"
@@ -20,9 +20,9 @@ ActiveRecord::Schema.define(version: 2021_10_26_180914) do
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-ActiveRecord::Schema.define(version: 2021_10_26_201311) do
+  end
 
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb3", force: :cascade do |t|
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
