c4af1e80353a75f36427b51736fc8e0cf716ac0c
KenH2021 <h_ken01@hotmail.com>
2021-10-29 14:08:36 -0400

    BackOfficeAuthentification

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_29_003600) do
+ActiveRecord::Schema.define(version: 2021_10_29_174731) do
 
   create_table "employees", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "Userid"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2021_10_29_003600) do
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: true
   end
 
   create_table "quote_pages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -64,6 +65,7 @@ ActiveRecord::Schema.define(version: 2021_10_29_003600) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
