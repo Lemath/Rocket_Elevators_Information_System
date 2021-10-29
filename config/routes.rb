3d70ed4d8a82ff1560c2de11fb91bd7839ae2a29
Anthonycyr <titocyr@gmail.com>
2021-10-29 18:20:07 -0400

    test commit

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
   get 'residential', to: 'application#residential'
   get 'commercial', to: 'application#commercial'
 
+
+
   #get 'back_office/index', to: 'back_office#index'
   get 'index',  to: 'application#index'
 end
