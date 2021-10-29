17c9ed90dc7fa9857406920042fa9052eb2dc1d5
SamaelTessier <samael.tessier@gmail.com>
2021-10-29 16:24:26 -0400

    Merge branch 'kenh-branch' into samael's-branch

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 ActiveRecord::Schema.define(version: 2021_10_29_172618) do
 
   create_table "employees", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
+ActiveRecord::Schema.define(version: 2021_10_29_174731) do
+
+  create_table "employees", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "Userid"
     t.string "first_name"
     t.string "last_name"
@@ -20,6 +23,7 @@ ActiveRecord::Schema.define(version: 2021_10_29_172618) do
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: true
   end
 
   create_table "quotes", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
@@ -30,6 +34,7 @@ ActiveRecord::Schema.define(version: 2021_10_29_172618) do
     t.string "total_price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  create_table "quote_pages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.boolean "residential"
     t.boolean "commercial"
     t.boolean "corporate"
@@ -37,6 +42,36 @@ ActiveRecord::Schema.define(version: 2021_10_29_172618) do
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
+    t.integer "number_appartments"
+    t.integer "number_floors"
+    t.integer "number_basement"
+    t.integer "number_parking"
+    t.integer "number_business"
+    t.integer "number_cages"
+    t.integer "number_corporation"
+    t.integer "number_occupant"
+    t.integer "number_hours"
+    t.boolean "standard"
+    t.boolean "premium"
+    t.boolean "excelium"
+    t.integer "elevator_amount"
+    t.integer "elevator_unit_price"
+    t.integer "elevator_total_price"
+    t.integer "installation_fees"
+    t.integer "final_price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "quotes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "numberappartments"
+    t.integer "numberfloors"
+    t.integer "numberbasement"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -44,6 +79,7 @@ ActiveRecord::Schema.define(version: 2021_10_29_172618) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
