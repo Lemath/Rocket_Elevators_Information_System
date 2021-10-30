de22302069edc469571eaaeaa097fcb8c75f8bbb
SamaelTessier <samael.tessier@gmail.com>
2021-10-29 20:53:33 -0400

    devise.rb login fix?

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -38,6 +38,13 @@ Devise.setup do |config|
   # available as additional gems.
   require 'devise/orm/active_record'
 
+
+
+  config.http_authenticatable = false
+
+
+
+  
   # ==> Configuration for any authentication mechanism
   # Configure which keys are used when authenticating a user. The default is
   # just :email. You can configure it to use [:username, :subdomain], so for
