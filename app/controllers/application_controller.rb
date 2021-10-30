cf9f18b2287bd9256cb71a9a622cc29bf10762eb
Lemath <lemathgagnon@hotmail.com>
2021-10-30 18:29:57 -0400

    database change number whatever

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,4 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery prepend: true
-    def index 
-        
-    end
-
-    def quote
-
-    end
-
-    def residential
-    end
-
-    def commercial
-    end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,17 @@
 Rails.application.routes.draw do
+ 
   resources :quotes
+  resources :employees
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  get 'quote/index', to: 'quote#index'
-
+  
   devise_for :user, :path => '', :path_names => { :sign_in => "login", :sign_out => "logout", :sign_up => "register" }
-
+  get 'home/index', to: 'home#index'
   root to: 'home#index'
 
-  resources :employees
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#index'
-  get 'quote', to: 'application#quote'
-  get 'residential', to: 'application#residential'
-  get 'commercial', to: 'application#commercial'
+  
+  get 'home/residential', to: 'home#residential'
+  get 'home/commercial', to: 'home#commercial'
 
 
-  #get 'back_office/index', to: 'back_office#index'
-  get 'index',  to: 'application#index'
+
 end
