81bf59ffaf5f24e1cb6013c5917282cab63f5ef4
SamaelTessier <samael.tessier@gmail.com>
2021-11-01 10:08:47 -0400

    fix to schema

--- db/schema.rb
+++ db/schema.rb
@@ -71,10 +71,7 @@ ActiveRecord::Schema.define(version: 2021_10_29_174731) do
     t.datetime "updated_at", null: false
   end
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 17c6c16e68b33770a9ac54d2136ffc4d318602f6
+
   create_table "quotes", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.string "amount_of_elevator"
     t.string "price_per_elevator"
@@ -91,12 +88,9 @@ ActiveRecord::Schema.define(version: 2021_10_29_174731) do
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
-<<<<<<< HEAD
-=======
-=======
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
->>>>>>> b411e2bf73a1c92077b90e361f0b8e1d63cdafd9
->>>>>>> 17c6c16e68b33770a9ac54d2136ffc4d318602f6
+
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
