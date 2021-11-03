f7f25093e64d3b14d956aefd94664d5dd8d0ab35
Anthonycyr <titocyr@gmail.com>
2021-11-03 10:54:47 -0400

    leads

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,10 @@
 class HomeController < ApplicationController
   def index
-    # @lead = Lead.find(params)
+
   end
   helper_method:lead
   def lead
   end
-  
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,17 +6,15 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   
   devise_for :user, :path => '', :path_names => { :sign_in => "login", :sign_out => "logout", :sign_up => "register" }
-  get 'home/index', to: 'home#index'
+  get 'home/index', to: 'home#index' 
+
   root to: 'home#index'
  
   post 'home/index',to: 'home#index'
+
   get 'home/residential', to: 'home#residential'
   get 'home/commercial', to: 'home#commercial'
 
-  post 'home/index',to: 'leads#create'
-
-  
-  
 
 
 
