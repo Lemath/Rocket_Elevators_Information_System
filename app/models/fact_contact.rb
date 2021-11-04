40c5f4e2d2b4d4bb0f0dfd90a72ec0176892ba4b
Lemath <lemathgagnon@hotmail.com>
2021-11-04 10:15:04 -0400

    extract and load method pretest

--- app/models/fact_contact.rb
+++ app/models/fact_contact.rb
@@ -1,2 +1,15 @@
 class FactContact < PostgresDbRecord
+  def getContactsData
+    contacts = Leads.where({ created_at: (Time.now - 1.day)..Time.now })
+    
+    contacts.each do |contact|
+      fact = FactContact.find_or_create_by(ContactId: contact.id)
+      fact.Craation_Date = contact.created_at
+      fact.Company_Name = contact.company_name
+      fact.Email = contact.email
+      fact.Project_Name = contact.project_name
+      fact.save
+      end
+    end
+  end
 end
--- app/models/fact_quote.rb
+++ app/models/fact_quote.rb
@@ -1,2 +1,14 @@
 class FactQuote < PostgresDbRecord
+
+  def getQuotesData
+    quotes = Quote.where({ created_at: (Time.now - 1.day)..Time.now })
+    quotes.each do |quote|
+      fact = FactQuote.find_or_create_by(QuoteId: quote.id)
+      fact.Creation_Date = quote.created_at
+      fact.Company_Name = quote.user.company_name
+      fact.Email = quote.user.email
+      fact.NbElevator = quote.amount_of_elevator
+      fact.save
+    end
+  end
 end
