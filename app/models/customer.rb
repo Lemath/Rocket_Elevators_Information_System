314e54ed6fff1ace396720fe1459c7356068e7c1
KenH2021 <h_ken01@hotmail.com>
2021-11-04 17:58:27 -0400

    CreationDate OK

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,6 +1,9 @@
 class Customer < ApplicationRecord
     belongs_to :address
-
+    after_initialize do
+        if new_record?
+          self.customer_creation_date ||= DateTime.now
+        end
     def create
         # @customer = current_user.posts.build(quote_params)
         @customer.user_id=current_user.id
@@ -9,7 +12,7 @@ class Customer < ApplicationRecord
     def customer_params
         params.require(:expense).permit(:user_id, :customer_creation_date, :company_name, :company_headquarter, :company_contact, :company_email, :company_description, :service_technical_authority_name, :technical_authority_phone, :service_technical_authority_email, :address_id)
 
-      end
+    end
 
-
+    end
 end
