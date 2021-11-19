d72f8571279364fec92dc5f49c96a7a25b82a414
Lemath <lemathgagnon@hotmail.com>
2021-11-19 10:52:38 -0500

    build message query

--- app/controllers/voice_report_controller.rb
+++ app/controllers/voice_report_controller.rb
@@ -1,6 +1,13 @@
 class VoiceReportController < ApplicationController
   def report
-    response = client.synthesize_speech input: {'text': 'hello World, how are you today?'}, voice: {'language_code': 'en-US'}, audio_config: {'audio_encoding': 'MP3'}
+    message = "Greetings to the logged user.
+    There are currently #{Elevator.count} elevators deployed in the #{Building.count} buildings of your #{Customer.count} customers.
+    Currently, #{Elevator.where.not(status: 'Intervention').count} elevators are not in Running Status and are being serviced.
+    You currently have #{Quote.count} quotes awaiting processing.
+    You currently have #{Lead.count} leads in your contact requests.
+    #{Battery.count} Batteries are deployed across #{Address.distinct.count(:city)} cities.
+    "
+    response = client.synthesize_speech input: {'text': message}, voice: {'language_code': 'en-US'}, audio_config: {'audio_encoding': 'MP3'}
     print response
     File.open Rails.root.join(
       'public',
