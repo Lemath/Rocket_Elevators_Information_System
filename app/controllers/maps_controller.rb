e91878bbc8394147e47a77b966f2e6989b858948
Anthonycyr <titocyr@gmail.com>
2021-11-18 23:20:38 -0500

    maps not bad !

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -5,6 +5,7 @@ class MapsController < ApplicationController
         @floors =[]
         @names=[]
         @nb_batteries= []
+        @nb_columns= []
         @nb_elevators = []
         @tech_name = []
 
@@ -13,24 +14,24 @@ class MapsController < ApplicationController
         x=0
         y=0
         z=0
-        building.address.all.each do |building|
+        @location.push(building.address.number_and_street)
+        @names.push(building.customer.company_contact)
+        @tech_name.push(building.name_technical_building)
+        x = x + building.batteries.count
+            building.batteries.all.each do |battery|
+                y = y + battery.columns.count
+                battery.columns.all.each do |column|
+                    f = f + column.number_of_floors_served
+                    z = z + column.elevators.count
+                    
 
-            @location.push(address.number_and_street)
-            
-            @names.push(building.customer.company_contact)
-            @tech_name.push(building.name_technical_building)
-            x = x + building.batteries.count
-                building.batteries.all.each do |battery|
-                    y = y + battery.columns.count
-                    @nb_batteries.push(y)
-                    battery.columns.all.each do |column|
-                        f = f + column.number_of_floors_served
-                        z = z + column.elevators.count
-                        @floors.push(f + y + f)
-                        @nb_elevators.push(f + y + z)
-                    end
                 end
+                @nb_columns.push(x)
+                @nb_batteries.push(y)
+                @floors.push(f)
+                @nb_elevators.push(z)
             end
+            
         end
         @location 
         @floors 
