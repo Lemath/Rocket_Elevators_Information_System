820346988a67a47583db8153f2fdccdbaad38b2d
KenH2021 <h_ken01@hotmail.com>
2021-11-19 11:49:46 -0500

    Slack Almost OK

--- app/models/elevator.rb
+++ app/models/elevator.rb
@@ -2,9 +2,17 @@ class Elevator < ApplicationRecord
     belongs_to :column
     # belongs_to :column, through: => :batteries
     # rescue_from StandardError do |exception|
-    after_save :trigger_sms_alerts, if: proc { saved_change_to_status?(to: 'Intervention') }
     after_save :send_change_to_slack, if: proc { saved_change_to_status? } 
+    after_save :trigger_sms_alerts, if: proc { saved_change_to_status?(to: 'Intervention') }
     
+
+
+def send_change_to_slack
+  notifier = Slack::Notifier.new ENV['SLACK_BOT_URL']
+  message = "The Elevator #{self.id} with Serial Number #{self.serial_number} changed status from #{self.status_before_last_save} to #{self.status}."
+  notifier.ping(message)
+end
+
 def trigger_sms_alerts
     alert_message = "#{self.column.battery.building.name_technical_building}
     the elevator located at #{self.column.battery.building.address.number_and_street} serial number:#{self.serial_number} need your intervention  "
